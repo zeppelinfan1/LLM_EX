3475a71ef9a5c3eccd2d9a968e8f42266d10cc6b
DanielC <danchiappori@gmail.com>
2025-01-22 16:25:56 -0500

    Finishing off preprocessing

--- text_preprocessing.py
+++ text_preprocessing.py
@@ -1,21 +1,53 @@
-import numpy as np
+import numpy as np, pandas as pd
+pd.set_option("display.width", 1000)
+pd.set_option("display.max_columns", 100)
+import os
 
 
-# Sample text
-text = "This is a string of text"
+"""PEPARING DF DATA
+"""
+
+# Text from Wine Kaggle DF
+df = pd.read_csv(r"C:\Users\DanielC\Desktop\winemag-data-130k-v2.csv", index_col=0)
+# Retain only relevent columns
+df = df[["description", "points"]]
+df["description"] = df["description"].str.lower()
+
+
+"""CREATING EMBEDDING
+"""
 
 # Step 1: Tokenize
-tokens = text.lower().split()
+full_tokens = []
+df["description"].apply(lambda x: [full_tokens.append(word) for word in x.split()])
+full_tokens.append("[EOS]")
+full_tokens.append("[PAD]")
 
 # Step 2: Build a vocabulary (The Word is the Key, the Index is the Value)
-vocab = {word: i for i, word in enumerate(set(tokens))}
+vocab = {word: i for i, word in enumerate(set(full_tokens))}
+
 
 # Step 3: Create an embedding matrix
 embedding_dim = 8
-embedding_matrix = np.random.randn(len(vocab), embedding_dim)
+embedding_matrix = np.random.randn(len(vocab), embedding_dim) # 2 extra: 1 for EOS, 1 for PAD
+
+# for word, index in vocab.items():
+#     print(f"Word: {word}, Index: {index}, Embedding: {embedding_matrix[index]}")
+
+
+"""APPLYING EMBEDDING TO DF
+"""
 
 # Step 4: Convert tokens to embedding vectors
-embedded_tokens = np.array([embedding_matrix[vocab[token]] for token in tokens])
+def apply_embedding(text, seq_length=10):
 
-for word, index in vocab.items():
-    print(f"Word: {word}, Index: {index}, Embedding: {embedding_matrix[index]}")
+    tokens = text.split() + ["[EOS]"]
+    tokens = tokens[:seq_length]
+    tokens += ["[PAD]"] * (seq_length - len(tokens))
+    embeddings = [embedding_matrix[vocab[token]].tolist() for token in tokens]
+
+    return embeddings
+
+df["embedding"] = df["description"].apply(apply_embedding)
+
+print(df["embedding"][0])
